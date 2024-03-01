366f1e00b3c5c43b7091730bbc88965e59a094b6
Alex <alexander.duz912dmail.com>
2024-03-01 15:39:02 +0100

    Creating superuser

--- talenthub/talenthub/projects/urls.py
+++ talenthub/talenthub/projects/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.projects, name="projects"),
-    path('projects-object/<str:pk>/', views.project, name="project"),
+    path('project/<slug:project_slug>/', views.project, name="project"),
+    # path('project-object/<str:pk>/', views.project, name="project"),
 ]
