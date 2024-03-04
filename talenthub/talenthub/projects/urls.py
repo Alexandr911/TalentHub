30eb2dc123b2ce5d67caf6aff92acda7efd7fbcb
Alexandr911 <60986783+Alexandr911@users.noreply.github.com>
2024-03-04 11:50:07 +0100

    urls.py aktualisieren

--- talenthub/talenthub/projects/urls.py
+++ talenthub/talenthub/projects/urls.py
@@ -3,5 +3,4 @@ from . import views
 urlpatterns = [
     path('', views.projects, name="projects"),
     path('project/<slug:project_slug>/', views.project, name="project"),
-    # path('project-object/<str:pk>/', views.project, name="project"),
 ]
