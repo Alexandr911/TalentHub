e9571d73a9450b0f3ff10779441bf2c3f7aec8e7
Alex <alexander.duz912dmail.com>
2024-03-15 07:52:45 +0100

    css

--- talenthub/talenthub/projects/forms.py
+++ talenthub/talenthub/projects/forms.py
@@ -1,8 +1,9 @@
 from django.forms import ModelForm
 from .models import Project
 
+
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        # fields = '__all__'
-        fields = ['title', 'slug', 'tags', 'description', 'demo_link', 'source_link']
+        fields = '__all__'
+        # fields = ['title', 'slug', 'tags', 'description', 'demo_link', 'source_link']
--- talenthub/talenthub/projects/views.py
+++ talenthub/talenthub/projects/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Project
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Project, Tag
+from .forms import ProjectForm
+
 
 # Create your views here.
 def projects(request):
@@ -7,7 +9,46 @@ def projects(request):
     context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
-def project(request, pk):
-    projectObj = Project.objects.get(id=pk)
-    tags = projectObj.tags.all()
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+
+def project(request, project_slug):
+    project = Project.objects.get(slug=project_slug)
+    tags = project.tags.all()
+    return render(request, 'projects/single-project.html', {'project': project})
+
+
+def createProject(request):
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('project')
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object': project}
+    return render(request, 'projects/delete.html')
+
+def projects_by_tag(request, tag_slug):
+    tag = get_object_or_404(Tag, slug=tag_slug)
+    projects = Project.objects.filter(tags__in=[tag])
+    context = {
+        "projects": projects
+    }
+
+    return render(request, "projects/projects.html", context)
